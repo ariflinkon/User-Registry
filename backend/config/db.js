48a1f7138c072cd6239d1cb87e47e7cb582cfafb
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2024-09-17 22:57:46 +0600

    Added .env file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'ueGcmZKzR*an[zzx',
-  database: 'user_management_db'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
